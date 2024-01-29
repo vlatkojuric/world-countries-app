3eb4dec747add2850b26251ac92127bdddfcb5b3
vlatkojuric <juricv78@gmail.com>
2024-01-29 14:00:52 +0100

    played around with the useQuery,but got into problems when trying to use the input to search specific countries ,so went back to the useEffect where I am a bit more experienced

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Country from "./components/Country";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import axios from "axios";
 
 function App() {
-  const client = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false,
-      },
-    },
-  });
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    axios.get("https://restcountries.com/v3.1/all").then((response) => {
+      setCountries(response.data);
+    });
+  }, []);
 
   return (
     <div className="App">
       <div className="countryDisplay">
-        <QueryClientProvider client={client}>
-          <Country />
-        </QueryClientProvider>
+        {countries.map((country) => (
+          <Country
+            key={country.cca2}
+            image={country.flags.png}
+            alt={country.flags.alt}
+            name={country.name.common}
+            region={country.region}
+            population={country.population}
+            capital={country.capital}
+          />
+        ))}
       </div>
     </div>
   );
--- src/components/Country.js
+++ src/components/Country.js
@@ -1,38 +1,18 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-
-export default function Country() {
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ["country-data"],
-    queryFn: async () => {
-      return axios
-        .get("https://restcountries.com/v3.1/all")
-        .then((response) => response.data);
-    },
-  });
-
-  if (isError) {
-    return <h1>Sorry,data can not be loaded at this time</h1>;
-  }
-
-  if (isLoading) {
-    return <h1>Data is loading...</h1>;
-  }
-
-  const countryData = data.map((country) => (
-    <div key={country.cca2}>
-      <img
-        src={country.flags.png}
-        alt={country.flags.alt}
-        height={200}
-        width={300}
-      />
-      <h3>{country.name.common}</h3>
-      <p>{country.region}</p>
-      <p>{country.population}</p>
-      <p>{country.capital}</p>
+export default function Country({
+  name,
+  image,
+  alt,
+  region,
+  population,
+  capital,
+}) {
+  return (
+    <div>
+      <img src={image} alt={alt} height={200} width={300} />
+      <h3>{name}</h3>
+      <p>{region}</p>
+      <p>{population}</p>
+      <p>{capital}</p>
     </div>
-  ));
-
-  return <div className="countryDisplay">{countryData}</div>;
+  );
 }
