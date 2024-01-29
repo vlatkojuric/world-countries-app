8f3f72ff2da6e482f43aacb0b6865a4f94ef5f10
vlatkojuric <juricv78@gmail.com>
2024-01-29 14:24:44 +0100

    added another state,when searching for a specific country in the input ,it shows the country,also made it responsive to small and big letters,put the reponse.data which was packed inside countries state,inside a variable,used the filter method to filter out the countries with big and small letters

--- src/App.js
+++ src/App.js
@@ -5,16 +5,49 @@ import axios from "axios";
 
 function App() {
   const [countries, setCountries] = useState([]);
-  useEffect(() => {
-    axios.get("https://restcountries.com/v3.1/all").then((response) => {
+  const [searchWord, setSearchWord] = useState("");
+
+  async function fetchCountries() {
+    const response = await axios.get("https://restcountries.com/v3.1/all");
+
+    if (response.status === 200) {
       setCountries(response.data);
-    });
+    } else {
+      console.error("Can not fetch data now :", response.status);
+    }
+  }
+
+  useEffect(() => {
+    fetchCountries();
   }, []);
+
+  const isLoading =
+    countries.length === 0 ? <h1>Data is loading ...</h1> : null;
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  const filteredCountries = countries?.filter((country) =>
+    country.name.common.toLowerCase().includes(searchWord.toLowerCase())
+  );
 
   return (
     <div className="App">
+      {isLoading}
+
+      <form onSubmit={handleSubmit}>
+        {!isLoading && (
+          <input
+            type="text"
+            placeholder="Search for a country..."
+            onChange={(event) => setSearchWord(event.target.value)}
+          />
+        )}
+      </form>
+
       <div className="countryDisplay">
-        {countries.map((country) => (
+        {filteredCountries?.map((country) => (
           <Country
             key={country.cca2}
             image={country.flags.png}
