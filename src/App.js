b87a18264030bb06fd8733cd4621ff5f223a1c33
vlatkojuric <juricv78@gmail.com>
2024-01-29 14:56:29 +0100

    back and foth between useEffect and useQuery,couldn't give it a rest,and finally found out what it was,I had to add the useEffect,save the fetche data into the setCountries function and add the dependancy array to prevent to many re-renders,second I added another state for the input ,then I created a variable where I filtered thru the countries variable and icluded the searchValue variable to react if the country name was found,also to react to lower case spelling

--- src/App.js
+++ src/App.js
@@ -1,63 +1,22 @@
-import { useEffect, useState } from "react";
 import "./App.css";
 import Country from "./components/Country";
-import axios from "axios";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
-  const [countries, setCountries] = useState([]);
-  const [searchWord, setSearchWord] = useState("");
-
-  async function fetchCountries() {
-    const response = await axios.get("https://restcountries.com/v3.1/all");
-
-    if (response.status === 200) {
-      setCountries(response.data);
-    } else {
-      console.error("Can not fetch data now :", response.status);
-    }
-  }
-
-  useEffect(() => {
-    fetchCountries();
-  }, []);
-
-  const isLoading =
-    countries.length === 0 ? <h1>Data is loading ...</h1> : null;
-
-  function handleSubmit(event) {
-    event.preventDefault();
-  }
-
-  const filteredCountries = countries?.filter((country) =>
-    country.name.common.toLowerCase().includes(searchWord.toLowerCase())
-  );
+  const client = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
 
   return (
     <div className="App">
-      {isLoading}
-
-      <form onSubmit={handleSubmit}>
-        {!isLoading && (
-          <input
-            type="text"
-            placeholder="Search for a country..."
-            onChange={(event) => setSearchWord(event.target.value)}
-          />
-        )}
-      </form>
-
-      <div className="countryDisplay">
-        {filteredCountries?.map((country) => (
-          <Country
-            key={country.cca2}
-            image={country.flags.png}
-            alt={country.flags.alt}
-            name={country.name.common}
-            region={country.region}
-            population={country.population}
-            capital={country.capital}
-          />
-        ))}
+      <div>
+        <QueryClientProvider client={client}>
+          <Country />
+        </QueryClientProvider>
       </div>
     </div>
   );
--- src/components/Country.js
+++ src/components/Country.js
@@ -1,18 +1,59 @@
-export default function Country({
-  name,
-  image,
-  alt,
-  region,
-  population,
-  capital,
-}) {
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function Country() {
+  const [searchValue, setSearchValue] = useState("");
+  const [countries, setCountries] = useState([]);
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["country-data"],
+    queryFn: async () => {
+      return axios
+        .get("https://restcountries.com/v3.1/all")
+        .then((response) => response.data);
+    },
+  });
+  useEffect(() => {
+    setCountries(data);
+  }, [data]);
+
+  if (isError) {
+    return <h1>Sorry,data can not be loaded at this time</h1>;
+  }
+
+  if (isLoading) {
+    return <h1>Data is loading...</h1>;
+  }
+
+  const filterCountries = countries?.filter((country) =>
+    country.name.common.toLowerCase().includes(searchValue.toLowerCase())
+  );
+
   return (
     <div>
-      <img src={image} alt={alt} height={200} width={300} />
-      <h3>{name}</h3>
-      <p>{region}</p>
-      <p>{population}</p>
-      <p>{capital}</p>
+      <div>
+        <input
+          type="text"
+          onChange={(event) => setSearchValue(event.target.value)}
+        />
+      </div>
+
+      <div className="countryDisplay">
+        {filterCountries?.map((country) => (
+          <div key={country.cca2}>
+            <img
+              src={country.flags.png}
+              alt={country.flags.alt}
+              height={200}
+              width={300}
+            />
+            <h3>{country.name.common}</h3>
+            <p>{country.region}</p>
+            <p>{country.population}</p>
+            <p>{country.capital}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
